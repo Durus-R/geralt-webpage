858d0476f34ce4ff53b7350393847027f561c114
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2024-02-15 10:50:33 +0100

    add recaptcha

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,4 +1,5 @@
 <template>
+  <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcQgXMpAAAAAHsOvQpUIRZ043YDb6d1l6xkTSvY"></script>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
@@ -15,6 +16,14 @@
       <router-view />
     </q-page-container>
   </q-layout>
+  <script>
+    function onClick(e) {
+      e.preventDefault();
+      grecaptcha.enterprise.ready(async () => {
+        const token = await grecaptcha.enterprise.execute('6LcQgXMpAAAAAHsOvQpUIRZ043YDb6d1l6xkTSvY', {action: 'LOGIN'});
+      });
+    }
+  </script>
 </template>
 
 <script setup lang="ts">

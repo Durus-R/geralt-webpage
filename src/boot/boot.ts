20c2f38e25021fa80b9180177b18fa306973d123
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2024-02-15 10:29:40 +0100

    add firebase

--- src/boot/boot.ts
+++ src/boot/boot.ts
@@ -1,10 +1,16 @@
 import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { Analytics} from "firebase/analytics"
 
 declare module '@vue/runtime-core' {
+
   interface ComponentCustomProperties {
     $axios: AxiosInstance;
     $api: AxiosInstance;
+    $app: FirebaseApp;
+    $analytics: Analytics
   }
 }
 
@@ -16,6 +22,20 @@ declare module '@vue/runtime-core' {
 // for each client)
 const api = axios.create({ baseURL: 'https://api.example.com' });
 
+const firebaseConfig = { //TODO make apiKey private
+  apiKey: "AIzaSyBvCYVmpQx4KV_DvLQ4frMUQtDV9wJPrn8",
+  authDomain: "geralt-counter.firebaseapp.com",
+  projectId: "geralt-counter",
+  storageBucket: "geralt-counter.appspot.com",
+  messagingSenderId: "381257378712",
+  appId: "1:381257378712:web:85a48cbe92d45084a359ed",
+  measurementId: "G-V1B02WSK7B"
+};
+
+// Initialize Firebase
+const fb_app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fb_app);
+
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
@@ -26,6 +46,9 @@ export default boot(({ app }) => {
   app.config.globalProperties.$api = api;
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
+
+  app.config.globalProperties.$app=fb_app;
+  app.config.globalProperties.$analytics=analytics;
 });
 
 export { api };

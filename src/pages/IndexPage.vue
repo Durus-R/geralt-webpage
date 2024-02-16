fa09f4fc31738a50fb02b85fc4c29afc037459c7
Durus-R <smartpower0105@gmail.com>
2024-02-16 15:23:31 +0100

    various changes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,22 +2,23 @@
 
   <q-page class="column items-center justify-center">
     <q-select v-model="select" :options="entries_names" label="Please select" style="min-width: 300px"></q-select>
-    <p style="margin: 30px">{{ count_text }}</p>
+    <p style="margin: 30px">{{ currentDoc?.count }}</p>
     <div class="row justify-around" style="margin: 40px">
-      <q-btn color="deep-orange" :label="button_text"
-             :disable="select === null" @click="button_click" style="margin-right: 25px"></q-btn>
-      <q-btn color="red" :label="-1" :disable="select === null || is_null" @click="button_decrement" rounded></q-btn>
+      <q-btn color="deep-orange" :label="currentDoc?.text"
+             :disable="select === null"  style="margin-right: 25px"></q-btn>
+      <q-btn color="red" :label="-1" :disable="select === null || currentDoc?.count === 0"  rounded></q-btn>
     </div>
 
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
-import { useCounterStore } from 'stores/counterstore';
-import { useFirestore, useCollection} from 'vuefire';
-import { collection, addDoc} from 'firebase/firestore';
+import { computed, Ref, ref } from 'vue';
+import { useFirestore, useCollection, useDocument } from 'vuefire';
+import { collection, doc, query, where, getDocs} from 'firebase/firestore';
+import { computedAsync } from '@vueuse/core/index';
 
+/*
 window.addEventListener('keydown', (e)=>{
   console.log(e.key)
   if (e.key=='End') {
@@ -25,67 +26,48 @@ window.addEventListener('keydown', (e)=>{
   } else if (e.key=='Delete') {
     button_decrement()
   }
-})
+})*/
 
 const fireStore = useFirestore();
 
+const entries_ref = collection(fireStore, 'people')
 
-
-const entries = useCollection(collection(fireStore, 'people'));
-
-// run firebase firestore:delete --all-collections to delete all data
-onMounted(async () => {await addDoc(collection(fireStore, 'people'), {
-  name: 'Hello',
-});})
+const entries = useCollection(entries_ref);
 
 const entries_names = computed(()=> {
   let res = []
   for (const e of entries.value) {
-    res.push(e.name)
+    res.push({
+      label: e.name,
+      value: e.id
+    })
   }
   return res;
 })
 
-console.log(entries.data)
-
-const store = useCounterStore();
-
-if (store.is_empty) {
-  store.push({
-    count: 0,
-    name: 'Geralt',
-    cite: 'Ehh...'
-  });
-  store.push({
-    count: 0,
-    name: 'Hoff',
-    cite: 'Sch*'
-  });
+interface option {
+  label: string,
+  value: string
 }
 
-const count_text = computed(() => {
-  if (select.value === null) {
-    return '';
-  }
-  return 'Count ' + store.get_single_name(select.value)?.count;
-});
+const select = ref() as Ref<option>;
 
+const currentDoc = useDocument(computed(()=>
+  select.value ?
+  doc(entries_ref, select.value.value) : null
+))
+
+
+/*
 const button_text = computed(() => {
   if (select.value === null) {
     return 'No entry selected.';
   }
   return store.get_single_name(select.value)?.cite;
-});
+});*/
 
-const is_null = computed(()=> {
-  if (select.value === null) {
-    return false
-  }
-  return (store.get_single_name(select.value)?.count ?? 0) < 1;
-})
 
-const select = ref(null);
-
+/*
 function button_click() {
   if (select.value === null) {
     return;
@@ -94,10 +76,10 @@ function button_click() {
 }
 
 function button_decrement() {
-  if (select.value === null || is_null.value) {
+  if (select.value === null || currentDoc.value.value?.count === 0) {
     return;
   }
   store.decrement(select.value);
-}
+}*/
 
 </script>

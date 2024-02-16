2abc2a6726475b3aa22c057809d8d53cb88200b1
Durus-R <smartpower0105@gmail.com>
2024-02-16 22:22:20 +0100

    got it to work

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,11 +2,12 @@
 
   <q-page class="column items-center justify-center">
     <q-select v-model="select" :options="entries_names" label="Please select" style="min-width: 300px"></q-select>
-    <p style="margin: 30px">{{ currentDoc?.count }}</p>
+    <p style="margin: 30px">{{ currentDoc? currentDoc.count : ""}}</p>
     <div class="row justify-around" style="margin: 40px">
-      <q-btn color="deep-orange" :label="currentDoc?.text"
-             :disable="select.value === ''"  style="margin-right: 25px"></q-btn>
-      <q-btn color="red" :label="-1" :disable="select.value === '' || currentDoc?.count === 0"  rounded></q-btn>
+      <q-btn color="deep-orange" :label="currentDoc? currentDoc.text : 'Select...'"
+             :disable="select.value === ''" @click="button_increment" style="margin-right: 25px"></q-btn>
+      <q-btn color="red" :label="-1" @click="button_decrement"
+             :disable="select.value === '' || (currentDoc? currentDoc.count : 0) === 0"  rounded></q-btn>
     </div>
 
   </q-page>
@@ -15,7 +16,7 @@
 <script setup lang="ts">
 import { computed, Ref, ref } from 'vue';
 import { useFirestore, useCollection, useDocument } from 'vuefire';
-import { collection, doc} from 'firebase/firestore';
+import { collection, updateDoc, doc} from 'firebase/firestore';
 
 
 /*
@@ -50,36 +51,41 @@ interface option {
   value: string
 }
 
-const select = ref() as Ref<option>;
+const select = ref({
+  label: '',
+  value: ''
+}) as Ref<option>;
 
-const currentDoc = useDocument(computed(()=>
-  select.value ?
-  doc(entries_ref, select.value.value) : null
-))
+const currentDocSource = computed(()=>
+  select.value.value ?
+    doc(entries_ref, select.value.value) : null
+)
 
-
-/*
-const button_text = computed(() => {
-  if (select.value === null) {
-    return 'No entry selected.';
-  }
-  return store.get_single_name(select.value)?.cite;
-});*/
+const currentDoc = useDocument(currentDocSource)
 
 
-/*
-function button_click() {
-  if (select.value === null) {
+async function button_add(addition : number) {
+  if (!(select.value.value && currentDocSource.value && currentDoc.value)) {
     return;
   }
-  store.increment(select.value);
+  await updateDoc(currentDocSource.value, {
+    count: currentDoc.value.count+addition
+  })
 }
 
+async function button_increment() {
+  return await button_add(1)
+}
+
+async function button_decrement() {
+  return await button_add(-1)
+}
+/*
 function button_decrement() {
   if (select.value === null || currentDoc.value.value?.count === 0) {
     return;
   }
-  store.decrement(select.value);
+  //store.decrement(select.value);
 }*/
 
 </script>

df1f5163a74fdd9fb4d1fd0bacf4ede7d0d96a69
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2024-02-22 08:12:26 +0100

    rename label, change style

--- src/components/CookiesSelectorComponent.vue
+++ src/components/CookiesSelectorComponent.vue
@@ -2,19 +2,27 @@
 h1 {
   font-size: 300%;
 }
+
 h2 {
   font-size: 140%;
 }
 </style>
 
 <template>
-    <q-scroll-area style="height: 500px; width: 700px;" @scroll="updateScroll">
-<div class="q-py-xs"><vue-markdown :source="md_text" /></div>
+  <q-scroll-area style="height: 500px; width: 700px;" @scroll="updateScroll">
+    <div class="q-py-xs">
+      <vue-markdown :source="md_text" />
+    </div>
 
-    </q-scroll-area>
+  </q-scroll-area>
   <div class="row">
-    <q-checkbox :disable="!scrolledToBottom" v-model="checked" label="Ich habe diese Informationen zur Kenntnis genommen und verstanden."></q-checkbox>
-    <q-btn :disable="!checked" label="Click here!" @click="enableCookies" />
+    <q-checkbox :disable="!scrolledToBottom"
+                style="margin-right: 50px"
+                v-model="checked"
+                label="Ich habe diese Informationen zur Kenntnis genommen und verstanden.">
+
+    </q-checkbox>
+    <q-btn :disable="!checked" label="Akzeptieren" @click="enableCookies" />
 
   </div>
 </template>
@@ -25,10 +33,10 @@ import VueMarkdown from 'vue-markdown-render';
 import { computedAsync } from '@vueuse/core';
 import { ref } from 'vue';
 
-const checked = ref(false)
-const scrolledToBottom = ref(false)
+const checked = ref(false);
+const scrolledToBottom = ref(false);
 
-function updateScroll(info : {verticalPercentage: number}) {
+function updateScroll(info: { verticalPercentage: number }) {
   scrolledToBottom.value = info.verticalPercentage > 0.95;
 }
 
